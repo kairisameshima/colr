c00121d640d31ba8884663f233e1321ac3c4d9b0
Kairi Sameshima <kairi@chicoryapp.com>
2017-11-10 17:31:36 -0500

    Init app files

---
+++ Colr/Colr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  colr
+//
+//  Created by Kairi Malcolm Sameshima on 11/10/17.
+//  Copyright © 2017 nyu.edu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
