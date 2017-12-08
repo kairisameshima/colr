7fc8345b4631e4571b33d223d2a5cf51ff50916e
Kairi Sameshima <kairi@chicoryapp.com>
2017-12-08 18:38:42 -0500

    New Game file colorV2

---
+++ colorV2/colorV2/GameScene.swift
@@ -0,0 +1,26 @@
+//
+//  GameScene.swift
+//  colorV2
+//
+//  Created by Kairi Malcolm Sameshima on 12/8/17.
+//  Copyright © 2017 nyu.edu. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        
+    }
+}
