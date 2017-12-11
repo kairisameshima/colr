7d2bb15149f2812b3613d81c94f20dde5b560432
Kairi Sameshima <kairi@chicoryapp.com>
2017-12-11 00:05:52 -0500

    modded files

--- colorV2/colorV2/GameViewController.swift
+++ colorV2/colorV2/GameViewController.swift
@@ -2,7 +2,7 @@
 //  GameViewController.swift
 //  colorV2
 //
-//  Created by Kairi Malcolm Sameshima on 12/8/17.
+//  Created by Kairi Malcolm Sameshima on 12/10/17.
 //  Copyright © 2017 nyu.edu. All rights reserved.
 //
 
@@ -11,29 +11,17 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
         // including entities and graphs.
-        let scene = GameScene(fileNamed: "GameScene")
-        let skView = view as! SKView
-        skView.showsFPS = true
-        skView.showsNodeCount = true
-        skView.ignoresSiblingOrder = true
-        scene?.scaleMode = .aspectFill
-        skView.presentScene(scene)
-
-//        if let scene = GKScene(fileNamed: "GameScene") {
-        
+        if let scene = GKScene(fileNamed: "MainMenuScene") {
             
             // Get the SKScene from the loaded GKScene
-<<<<<<< HEAD
-            if let sceneNode = scene.rootNode as! GameScene? {
+            if let sceneNode = scene.rootNode as! MainMenuScene? {
                 
-                // Copy gameplay related content over to the scene
-                // sceneNode.entities = scene.entities
-                // sceneNode.graphs = scene.graphs
                 
                 // Set the scale mode to scale to fit the window
                 sceneNode.scaleMode = .aspectFill
@@ -41,7 +29,7 @@ class GameViewController: UIViewController {
                 // Present the scene
                 if let view = self.view as! SKView? {
                     view.presentScene(sceneNode)
-                    view.isPaused = false
+                    
                     view.ignoresSiblingOrder = true
                     
                     view.showsFPS = true
@@ -49,34 +37,12 @@ class GameViewController: UIViewController {
                 }
             }
         }
-=======
-//            if let sceneNode = scene.rootNode as! GameScene? {
-//
-//                // Copy gameplay related content over to the scene
-////                sceneNode.entities = scene.entities
-////                sceneNode.graphs = scene.graphs
-//
-//                // Set the scale mode to scale to fit the window
-//                sceneNode.scaleMode = .aspectFill
-//
-//                // Present the scene
-//                if let view = self.view as! SKView? {
-//                    view.presentScene(sceneNode)
-//
-//                    view.ignoresSiblingOrder = true
-//
-//                    view.showsFPS = true
-//                    view.showsNodeCount = true
-//                }
-//            }
-//        }
->>>>>>> 4cc732318fb4683dafb9c19997f0e088955d9148
     }
-
+    
     override var shouldAutorotate: Bool {
         return true
     }
-
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if UIDevice.current.userInterfaceIdiom == .phone {
             return .allButUpsideDown
@@ -84,13 +50,14 @@ class GameViewController: UIViewController {
             return .all
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
 }
+
--- colorV2/colorV2/MainMenuScene.swift
+++ colorV2/colorV2/MainMenuScene.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+
+import SpriteKit
+
+class MainMenuScene: SKScene {
+    
+    /* UI Connections */
+    var buttonPlay: MSButtonNode!
+    
+    override func didMove(to view: SKView) {
+        /* Setup your scene here */
+        
+        /* Set UI connections */
+        buttonPlay = self.childNode(withName: "buttonPlay") as! MSButtonNode
+        
+        /* Setup button selection handler */
+        buttonPlay.selectedHandler = { [unowned self] in
+            
+            if let view = self.view {
+                
+                // Load the SKScene from 'GameScene.sks'
+                if let scene = SKScene(fileNamed: "GameScene") {
+                    
+                    // Set the scale mode to scale to fit the window
+                    scene.scaleMode = .aspectFill
+                    
+                    // Present the scene
+                    view.presentScene(scene)
+                }
+                
+                // Debug helpers
+                view.isPaused = false
+                view.showsFPS = true
+                view.showsPhysics = true
+                view.showsDrawCount = true
+            }
+        }
+        
+    }
+}
+
