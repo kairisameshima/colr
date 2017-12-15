5c3dce0b440df4751eb62a325a5d076ec5a2bed2
DanielMZhu <thisisdanielzhu@gmail.com>
2017-12-15 10:56:23 -0500

    sound and stuff

--- colorV2/colorV2/GameScene.swift
+++ colorV2/colorV2/GameScene.swift
@@ -10,9 +10,9 @@ import SpriteKit
 import GameplayKit
 import SpriteKit
 import UIKit
-
+import AVFoundation
 class GameScene: SKScene {
-    
+    var soundEffect: AVAudioPlayer!
     //chameleon node
     //let theBlueOne: SKSpriteNode = childNode(withName: "blue") as! SKSpriteNode
     
@@ -41,24 +41,33 @@ class GameScene: SKScene {
     var goodRedAngle = CGFloat(0.0)
     var goodGreenAngle = CGFloat(0.0)
     var vectorAngle = CGFloat(0.0)
-    
+    var changedColor = false
+    var hitRed = false
+    var hitBlue = false
+    var hitGreen = false
     
     var fernBase: SKSpriteNode!
     var fernBase2: SKSpriteNode!
-    
+    var apple: SKSpriteNode!
+    var banana: SKSpriteNode!
+    var blueberry: SKSpriteNode!
     var interval = 32.0
     var interval2 = 32
     var steve: SKSpriteNode!
     var message: SKLabelNode!
     var resetButton: SKLabelNode!
     var scoreLabel: SKLabelNode!
-    
+    var finalX = 0.0
+    var finalY = 0.0
     var score = 0
     
     override func didMove(to view: SKView) {
         fernBase = childNode(withName: "ferns") as! SKSpriteNode
         fernBase2 = childNode(withName: "ferns_2") as! SKSpriteNode
-
+        let apple: SKSpriteNode = childNode(withName: "red") as! SKSpriteNode
+        let banana: SKSpriteNode = childNode(withName: "green") as! SKSpriteNode
+        let blueberry: SKSpriteNode = childNode(withName: "blue") as! SKSpriteNode
+        
         let steve: SKSpriteNode = childNode(withName: "steve") as! SKSpriteNode
         tongue = childNode(withName: "tongue") as! SKSpriteNode
         print("ok made this")
@@ -119,14 +128,17 @@ class GameScene: SKScene {
                 }
                 
                 if touchedNode.name == "redMinus" {
+                     playSoundWith(fileName: "shed", fileExtension: "mp3")
                     RED -= interval
                 }
                 
                 if touchedNode.name == "greenMinus" {
+                    playSoundWith(fileName: "shed", fileExtension: "mp3")
                     GREEN -= interval
                 }
 
                 if touchedNode.name == "blueMinus" {
+                     playSoundWith(fileName: "shed", fileExtension: "mp3")
                     BLUE -= interval
                 }
                 if touchedNode.name == "playAgain" {
@@ -152,18 +164,19 @@ class GameScene: SKScene {
         changeTongueLength()
         changeSteveColor()
         displayScore()
+        respawnFruit()
     }
     func calculateTongue(startPoint: CGPoint, endPoint: CGPoint){
+        playSoundWith(fileName: "swish", fileExtension: "mp3")
         let diffX = startPoint.x - endPoint.x
         let diffY = startPoint.y - endPoint.y
         let hypot = sqrt(diffX*diffX+diffY*diffY)
         desiredAngle = atan(diffX/diffY)
         print("THIS IS THE DESIRED ANGLE ",desiredAngle)
-        desiredLength = 2.5*hypot
+        desiredLength = 3*hypot
         tongue.zRotation = (-desiredAngle)
     }
     func changeTongueLength(){
-        
         print("changing tongue length")
         let tongue: SKSpriteNode = childNode(withName: "tongue") as! SKSpriteNode
         print("TONGUE ANGLE: ",tongue.zRotation)
@@ -178,33 +191,77 @@ class GameScene: SKScene {
         else if(actualLength>desiredLength && !retracting){//reached max length
             retracting = true
             print("reached max length")
-            let finalX = cos(0.5*3.14159265-desiredAngle)*desiredLength+tongue.position.x//wouldnt let me use double.pi too lazy to circumvent programmatically
-            let finalY = sin(0.5*3.14159265-desiredAngle)*desiredLength+tongue.position.y
+            finalX = Double(cos(0.5*3.14159265-desiredAngle)*desiredLength+tongue.position.x)//wouldnt let me use double.pi too lazy to circumvent programmatically
+           finalY = Double(sin(0.5*3.14159265-desiredAngle)*desiredLength+tongue.position.y)
             print("Starting coordinates: ",tongue.position.x,",",tongue.position.y)
             print("Final coordinates: ",finalX,",",finalY)
             print("are we retracting: ",retracting)
             if(finalX > -200 && finalX < -120 && finalY < 475 && finalY > 398){
-                RED+=interval
+                //RED+=interval
+                playSoundWith(fileName: "pop", fileExtension: "mp3")
+                changedColor = true
+                hitRed = true
                 print("hit red")
             }
             else if(finalX < 80 && finalX > 5 && finalY > 242 && finalY < 360){
-                    GREEN+=interval
+                //GREEN+=interval
+                hitGreen = true
+                changedColor = true
+                playSoundWith(fileName: "pop", fileExtension: "mp3")
                     print("hit green")
                 }
             
             else if( finalX > 223 && finalX < 320 && finalY > 400 && finalY < 490){
-            BLUE+=interval
-            print("hit blue")
+                //BLUE+=interval
+                hitBlue = true
+                changedColor = true
+                playSoundWith(fileName: "pop", fileExtension: "mp3")
+                print("hit blue")
             
             }
         }
         else if(retracting && actualLength>15){
+            if(hitRed||hitBlue||hitGreen){
+                moveFruit()
+            }
+            
             print("are we retracting: ",retracting)
             actualLength = actualLength-15
             print("subtracting 5")
             tongue.size.height = actualLength
+            if(actualLength<=15 && changedColor){
+                playSoundWith(fileName: "bling", fileExtension: "mp3")
+                changedColor = false
+            }
         }
         else if(actualLength<=15){
+            if(hitRed){
+                let apple: SKSpriteNode = childNode(withName: "red") as! SKSpriteNode
+                apple.size.width = 0
+                apple.size.height = 0
+                apple.position.x = -160
+                apple.position.y = 484.211
+                RED+=interval
+                hitRed = false
+            }
+            if(hitBlue){
+                let blueberry: SKSpriteNode = childNode(withName: "blue") as! SKSpriteNode
+                blueberry.size.width = 0
+                blueberry.size.height = 0
+                blueberry.position.x = 268.8
+                blueberry.position.y = 494.45
+                BLUE+=interval
+                hitBlue = false
+            }
+            if(hitGreen){
+                let banana: SKSpriteNode = childNode(withName: "green") as! SKSpriteNode
+                banana.size.width = 0
+                banana.size.height = 0
+                banana.position.x = 41.128
+                banana.position.y = 355.55
+                GREEN+=interval
+                hitGreen = false
+            }
             print("are we retracting: ",retracting)
             actualLength = 0
             print("stop moving")
@@ -250,7 +307,7 @@ class GameScene: SKScene {
         
         let messageLocation =  CGPoint(x: 45, y: 141)
         message.run(SKAction.move(to: messageLocation, duration: 2.5))
-        
+        playSoundWith(fileName: "win", fileExtension: "mp3")
         // Drop down the play again button
         let resetButtonLocation =  CGPoint(x: 45, y: 81)
         resetButton.run(SKAction.move(to: resetButtonLocation, duration: 2))
@@ -305,5 +362,52 @@ class GameScene: SKScene {
         BASE *= interval2
         print(BASE)
         return BASE;
+    }
+    func playSoundWith(fileName: String, fileExtension: String){
+        let audioSourceURL: URL!
+        audioSourceURL = Bundle.main.url(forResource: fileName, withExtension: fileExtension)
+        do{
+            soundEffect = try AVAudioPlayer.init(contentsOf: audioSourceURL!)
+            soundEffect.prepareToPlay()
+            soundEffect.setVolume(1, fadeDuration: 0)
+            soundEffect.play()
+        }
+        catch{
+            print("darn")
+        }
+    }
+    func moveFruit(){
+        if(hitRed){
+             let apple: SKSpriteNode = childNode(withName: "red") as! SKSpriteNode
+            apple.position.x-=CGFloat(CGFloat(finalX)-tongue.position.x)/CGFloat(Int(desiredLength/15+desiredLength.truncatingRemainder(dividingBy: 15)))
+            apple.position.y-=CGFloat(CGFloat(finalY)-tongue.position.y)/CGFloat(Int(desiredLength/15+desiredLength.truncatingRemainder(dividingBy: 15)))
+        }
+        else if(hitBlue){
+            let blueberry: SKSpriteNode = childNode(withName: "blue") as! SKSpriteNode
+            blueberry.position.x-=CGFloat(CGFloat(finalX)-tongue.position.x)/CGFloat(Int(desiredLength/15+desiredLength.truncatingRemainder(dividingBy: 15)))
+            blueberry.position.y-=CGFloat(CGFloat(finalY)-tongue.position.y)/CGFloat(Int(desiredLength/15+desiredLength.truncatingRemainder(dividingBy: 15)))
+        }
+        else if(hitGreen){
+            let banana: SKSpriteNode = childNode(withName: "green") as! SKSpriteNode
+            banana.position.x-=CGFloat(CGFloat(finalX)-tongue.position.x)/CGFloat(Int(desiredLength/15+desiredLength.truncatingRemainder(dividingBy: 15)))
+            banana.position.y-=CGFloat(CGFloat(finalY)-tongue.position.y)/CGFloat(Int(desiredLength/15+desiredLength.truncatingRemainder(dividingBy: 15)))
+        }
+    }
+    func respawnFruit(){
+        let apple: SKSpriteNode = childNode(withName: "red") as! SKSpriteNode
+        let blueberry: SKSpriteNode = childNode(withName: "blue") as! SKSpriteNode
+        let banana: SKSpriteNode = childNode(withName: "green") as! SKSpriteNode
+        if(apple.size.width<102.4){
+            apple.size.width+=10.204/2.0
+            apple.size.height+=10.204/2.0
+        }
+        else if(blueberry.size.width<102.4){
+            blueberry.size.width+=10.204/2.0
+            blueberry.size.height+=10.204/2.0
+        }
+        else if(banana.size.width<102.4){
+            banana.size.width+=10.204/2.0
+            banana.size.height+=10.204/2.0
+        }
     }
 }
